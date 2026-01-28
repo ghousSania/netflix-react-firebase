6e48d577266cebf7eac22f40ad53e3a9ae73dd00
Sania Ghous <ghoussania806@gmail.com>
2026-01-28 13:04:20 +0500

    security: rotate firebase api key and use environment variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-zEc4E-9sPr1UEOT5w2sXGC2LbRAjwwI",
+  apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
   authDomain: "netflix-react-firebase-98266.firebaseapp.com",
   projectId: "netflix-react-firebase-98266",
   storageBucket: "netflix-react-firebase-98266.firebasestorage.app",
   messagingSenderId: "1019965165897",
   appId: "1:1019965165897:web:ae2f30020db3fbbe418a98",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
